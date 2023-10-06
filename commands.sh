99548d71a292963ec7549ebcb040668939652f24
William de Sousa Costa <37248765+WilliamdeSousa@users.noreply.github.com>
2023-10-06 17:40:35 -0300

    Update and rename lista.sh to commands.sh

--- commands.sh
+++ commands.sh
@@ -13,3 +13,14 @@ run()
 {
 	gcc $1 -o $1.run -lm && ./$1.run && rm $1.run
 }
+
+runproject()
+{
+	for arquivo in `ls *.c | grep -v main.c`; do
+	    gcc -c $arquivo
+	done
+	gcc `ls *.o` main.c -o main
+	./main
+	rm `ls *.o` main
+}
+	
